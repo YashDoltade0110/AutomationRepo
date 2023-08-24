284148eb5d19ac875814764721ada181039cf903
YashDoltade0110 <143082491+YashDoltade0110@users.noreply.github.com>
2023-08-24 16:11:21 +0530

    Add files via upload

---
+++ SeleniumDemo.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class SeleniumDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+        Webdriver driver = new ChromeDriver();
+    }
+}
